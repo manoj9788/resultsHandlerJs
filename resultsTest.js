2683c257db5fb0466c67d0c969611a5530e3dece
isonic1 <justin@Justins-MBP.home>
2017-07-14 18:46:36 -0400

    initial commit

---
+++ resultsTest.js
@@ -0,0 +1,35 @@
+var ApplitoolsTestResultHandler = require('./aptest.js').ApplitoolsTestResultHandler;
+
+const {Builder, By, until} = require('selenium-webdriver');
+var path = require('chromedriver').path;
+var driver = new Builder().forBrowser('chrome').build();
+var Eyes = require('eyes.selenium').Eyes;
+var eyes = new Eyes();
+
+var applitoolsKey = 'your applitools key'
+eyes.setApiKey(applitoolsKey);
+
+var applitoolsViewKey = 'your applitools view key'
+
+var downloadDir = 'your download directory' //e.g. /Users/images
+
+try {
+  eyes.open(driver, 'Applitools', 'Test Web Page', {width: 900, height: 600}).then(function(driver) {
+    driver.get('https://applitools.com/helloworld');
+    eyes.checkWindow('Main Page');
+    driver.findElement(By.tagName('button')).click();
+    eyes.checkWindow('Click!!!');
+    }).then(function(){
+      return eyes.close(false);
+    }).then(function(testResult){
+      var handler= new ApplitoolsTestResultHandler(testResult, applitoolsViewKey);
+      
+      handler.downloadImages(downloadDir, 'baseline'); //valid types = baseline, current, diff
+      
+      var testStatus = handler.stepStatusArray();
+      console.log("My Test Status: " + testStatus)
+    });
+} finally {
+  driver.quit();
+  eyes.abortIfNotClosed();
+}
